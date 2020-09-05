087b8d640a37dd3b0544b82c2bb9e8bbd0db6491
Dylan Turner <dylantdmt@gmail.com>
2020-09-05 03:53:54 -0500

    Rearranged helper functions in lexer

--- src/parser/lexer.c
+++ src/parser/lexer.c
@@ -5,21 +5,54 @@
 #include <token.h>
 #include <lexer.h>
 
-token_list_t lexBoolVal(int *ref_i, string_t str, token_list_t list) {
+token_list_t fromStringHelper(token_list_t list, string_t str) {
     token_list_t new_list = list;
-    int start_ind = *ref_i;
 
-    if(str.c_str[*ref_i] == 't') {
-        *ref_i += 3;
-
-        token_t new_tok = (token_t) { string.fromCharArray("true"), start_ind, BOOL_VAL };
-        new_list = tokenizer.appendToken(new_tok, new_list);
-    } else {
-        *ref_i += 4;
-
-        token_t new_tok = (token_t) { string.fromCharArray("false"), start_ind, BOOL_VAL };
-        new_list = tokenizer.appendToken(new_tok, new_list);
+    for(int i = 0; i < str.length; i++) {
+        if(str.c_str[i] == ' ' || str.c_str[i] == '\r'
+                || str.c_str[i] == '\n' || str.c_str[i] == '\t') {
+            i++;
+        } if(str.c_str[i] == '\'') {
+            new_list = lexString(&i, str, new_list);
+        } else if(str.c_str[i] == '.' || (str.c_str[i] >= '0' && str.c_str[i] <= '9')) {
+            new_list = lexNumber(&i, str, new_list);
+        } else if(i + 3 < str.length && 
+                (strncmp("true", str.c_str + i, 4) == 0 || strncmp("false", str.c_str + i, 5) == 0)) {
+            new_list = lexBoolVal(&i, str, new_list);
+        }
     }
+
+    return new_list;
+}
+
+token_list_t lexString(int *ref_i, string_t str, token_list_t list) {
+    token_list_t new_list = list;
+    
+    int start_ind = *ref_i;
+    string_t string_literal = string.instance();
+    string_literal = string.appendChar('\'', string_literal);
+    (*ref_i)++;
+    while(*ref_i < str.length) {
+        if(str.c_str[*ref_i] == '\\') {
+            string_literal = string.appendChar(str.c_str[(*ref_i)++], string_literal);
+            if(*ref_i >= str.length) {
+                printf("Parse error: Unterminated escape sequence in string literal!");
+                exit(-1);
+            }
+
+            string_literal = string.appendChar(str.c_str[*ref_i], string_literal);
+        } else if(str.c_str[*ref_i] == '\'') {
+            string_literal = string.appendChar(str.c_str[(*ref_i)++], string_literal);
+            break;
+        } else {
+            string_literal = string.appendChar(str.c_str[*ref_i], string_literal);
+        }
+        (*ref_i)++;
+    }
+    (*ref_i)--;
+
+    token_t new_tok = (token_t) { string_literal, start_ind, STRING };
+    new_list = tokenizer.appendToken(new_tok, new_list);
     return new_list;
 }
 
@@ -71,53 +104,20 @@ token_list_t lexNumber(int *ref_i, string_t str, token_list_t list) {
     return new_list;
 }
 
-token_list_t lexString(int *ref_i, string_t str, token_list_t list) {
+token_list_t lexBoolVal(int *ref_i, string_t str, token_list_t list) {
     token_list_t new_list = list;
-    
     int start_ind = *ref_i;
-    string_t string_literal = string.instance();
-    string_literal = string.appendChar('\'', string_literal);
-    (*ref_i)++;
-    while(*ref_i < str.length) {
-        if(str.c_str[*ref_i] == '\\') {
-            string_literal = string.appendChar(str.c_str[(*ref_i)++], string_literal);
-            if(*ref_i >= str.length) {
-                printf("Parse error: Unterminated escape sequence in string literal!");
-                exit(-1);
-            }
 
-            string_literal = string.appendChar(str.c_str[*ref_i], string_literal);
-        } else if(str.c_str[*ref_i] == '\'') {
-            string_literal = string.appendChar(str.c_str[(*ref_i)++], string_literal);
-            break;
-        } else {
-            string_literal = string.appendChar(str.c_str[*ref_i], string_literal);
-        }
-        (*ref_i)++;
+    if(str.c_str[*ref_i] == 't') {
+        *ref_i += 3;
+
+        token_t new_tok = (token_t) { string.fromCharArray("true"), start_ind, BOOL_VAL };
+        new_list = tokenizer.appendToken(new_tok, new_list);
+    } else {
+        *ref_i += 4;
+
+        token_t new_tok = (token_t) { string.fromCharArray("false"), start_ind, BOOL_VAL };
+        new_list = tokenizer.appendToken(new_tok, new_list);
     }
-    (*ref_i)--;
-
-    token_t new_tok = (token_t) { string_literal, start_ind, STRING };
-    new_list = tokenizer.appendToken(new_tok, new_list);
-    return new_list;
-}
-
-token_list_t fromStringHelper(token_list_t list, string_t str) {
-    token_list_t new_list = list;
-
-    for(int i = 0; i < str.length; i++) {
-        if(str.c_str[i] == ' ' || str.c_str[i] == '\r'
-                || str.c_str[i] == '\n' || str.c_str[i] == '\t') {
-            i++;
-        } if(str.c_str[i] == '\'') {
-            new_list = lexString(&i, str, new_list);
-        } else if(str.c_str[i] == '.' || (str.c_str[i] >= '0' && str.c_str[i] <= '9')) {
-            new_list = lexNumber(&i, str, new_list);
-        } else if(i + 3 < str.length && 
-                (strncmp("true", str.c_str + i, 4) == 0 || strncmp("false", str.c_str + i, 5) == 0)) {
-            new_list = lexBoolVal(&i, str, new_list);
-        }
-    }
-
     return new_list;
 }

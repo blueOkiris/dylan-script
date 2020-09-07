c7d3e52c63d039f550ffa798c36590e356d3699b
Dylan Turner <dylantdmt@gmail.com>
2020-09-07 11:38:47 -0400

    Added parsing of function definitions

--- src/Output.cs
+++ src/Output.cs
@@ -31,7 +31,97 @@ namespace dylanscript {
 
         private static CompoundToken parseFuncDef(
                 ref int i, SymbolToken[] tokens) {
-            return new CompoundToken(TokenType.Program, new Token[0] {});
+            i++;
+            if(i >= tokens.Length) {
+                throw new Exception(
+                    "Parser Error: "
+                    + "Unexpected EOF at line " + tokens[i - 1].Line
+                    + ", pos " + tokens[i - 1].Pos + "."
+                );
+            }
+            if(tokens[i].Type != TokenType.Name) {
+                throw new Exception(
+                    "Parser Error: "
+                    + "Expected new name after keyword 'struct' at line "
+                    + tokens[i].Line + ", pos: " + tokens[i].Pos + "."
+                );
+            }
+            var name = tokens[i];
+
+            i++;
+            if(i >= tokens.Length) {
+                throw new Exception(
+                    "Parser Error: "
+                    + "Unexpected EOF at line " + tokens[i - 1].Line
+                    + ", pos " + tokens[i - 1].Pos + "."
+                );
+            }
+            if(tokens[i].Type != TokenType.Parenth
+                    || tokens[i].Source != "(") {
+                throw new Exception(
+                    "Parser Error: "
+                    + "Expected new name after keyword 'struct' at line "
+                    + tokens[i].Line + ", pos: " + tokens[i].Pos + "."
+                );
+            }
+            var lpar = tokens[i];
+
+            i++;
+            if(i >= tokens.Length) {
+                throw new Exception(
+                    "Parser Error: "
+                    + "Unexpected EOF at line " + tokens[i - 1].Line
+                    + ", pos " + tokens[i - 1].Pos + "."
+                );
+            }
+
+            CompoundToken args = null;
+            if(tokens[i].Type != TokenType.Parenth) {
+                args = parseDefArgBody(ref i, tokens);
+            }
+
+            if(tokens[i].Type != TokenType.Parenth
+                    || tokens[i].Source != ")") {
+                throw new Exception(
+                    "Parser Error: "
+                    + "Expected new name after keyword 'struct' at line "
+                    + tokens[i].Line + ", pos: " + tokens[i].Pos + "."
+                );
+            }
+            var rpar = tokens[i];
+
+            i++;
+            if(i >= tokens.Length) {
+                throw new Exception(
+                    "Parser Error: "
+                    + "Unexpected EOF at line " + tokens[i - 1].Line
+                    + ", pos " + tokens[i - 1].Pos + "."
+                );
+            }
+            if(tokens[i].Type != TokenType.RetDerefOp) {
+                throw new Exception(
+                    "Parser Error: "
+                    + "Expected new name after keyword 'struct' at line "
+                    + tokens[i].Line + ", pos: " + tokens[i].Pos + "."
+                );
+            }
+            var retOp = tokens[i];
+
+            var type = parseTypeName(ref i, tokens);
+            var body = parseScopeDec(ref i, tokens);
+
+            var children = new List<Token>();
+            children.Add(name);
+            children.Add(lpar);
+            if(args != null) {
+                children.Add(args);
+            }
+            children.Add(rpar);
+            children.Add(retOp);
+            children.Add(type);
+            children.Add(body);
+
+            return new CompoundToken(TokenType.FuncDef, children.ToArray());
         }
 
         private static CompoundToken parseImport(
